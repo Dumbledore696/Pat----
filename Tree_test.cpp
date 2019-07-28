c62d7fc8fdd0080227096f5d34a6ac31021364a9
Dumbledore696 <1362371931@qq.com>
2019-07-28 12:06:52 +0800

    增加用递归求普通二叉树求wpl

--- Tree_test.cpp
+++ Tree_test.cpp
@@ -1,17 +1,30 @@
 #include<cstdio>
 #include<iostream>
 #include<stack>
+#include<queue>
 #include<algorithm>
 using namespace std;
 
 int num=1;
-int n=5;
+int n=6;
 
 typedef struct BTNode{
 	int data;
 	struct BTNode *lchild;
 	struct BTNode *rchild;
+	//int layer;           //getwidth1
 }BTNode;
+typedef struct BTreelayer{ //getwidth2
+	BTNode *p;
+	int layer;
+}BTreelayer;
+
+/* typedef struct BTNode2{
+	int data;
+	struct BTNode2 *lchild;
+	struct BTNode2 *rchild;
+	int layer;
+}BTNode2; */
 
 void insert(BTNode *&root,int x){
 	if(root==NULL){
@@ -74,7 +87,7 @@ void postordernorecursion(BTNode *bt){//二叉树后序非递归遍历
 	}
 }
 
-void printancestorpath(BTNode *bt,int k){//打印k的所有祖先
+void printancestorpath(BTNode *bt,int k){//打印k的所有祖先//后序非递归遍历
 	                                     //在出栈时判断是否为k，只要写一次判断，
                                          //但要先将自己出栈才能打印路径
 										 //在入栈时判断要写两次判断
@@ -115,6 +128,7 @@ void printancestorpath(BTNode *bt,int k){//打印k的所有祖先
 void nearestcommonancestor(BTNode *bt,int k,int q){//找到两个节点最近的公共祖先节点
 	                                               //在出栈时判断是否为p或k，
                                                    //这样k一定在q前面，而入栈时不一定
+												   //后序非递归遍历
 	BTNode* s[10];
 	BTNode* s1[10];
 	int top=-1;
@@ -157,12 +171,106 @@ void nearestcommonancestor(BTNode *bt,int k,int q){//找到两个节点最近的
 		}
 	}
 }
+
+int getwidth(BTNode *bt){//求二叉树的宽度
+	queue<BTNode*>q;
+	bt->layer=1;
+	int layer[10];
+	memset(layer,0,sizeof(layer));
+	q.push(bt);
+	while(!q.empty()){
+		bt=q.front();
+		q.pop();
+		layer[bt->layer]++;
+		if(bt->lchild!=NULL){
+			bt->lchild->layer=bt->layer+1;
+			q.push(bt->lchild);
+		}
+		if(bt->rchild!=NULL){
+			bt->rchild->layer=bt->layer+1;
+			q.push(bt->rchild);
+		}
+	}
+	int maxwidth=-1;
+	for(int i=1;i<=bt->layer;i++){
+		if(layer[i]>maxwidth){
+			maxwidth=layer[i];
+		}
+	}
+	return maxwidth;
+	
+	
+}
+
+int getwidth2(BTNode *root){//不改变树节点类型的情况下改变队列节点类型增加layer
+	queue<BTreelayer *>q;
+	BTreelayer * bt=new BTreelayer;
+	bt->p=root;
+	bt->layer=1;
+	int layer[10];
+	memset(layer,0,sizeof(layer));
+	q.push(bt);
+	while(!q.empty()){
+		bt=q.front();
+		q.pop();
+		BTNode* temp;
+		temp=bt->p;
+		int k=bt->layer;
+		layer[k]++;
+		if(temp->lchild!=NULL){
+			BTreelayer* temp2=new BTreelayer;//使用前别忘记初始化
+			temp2->layer=k+1;
+			temp2->p=temp->lchild;
+			q.push(temp2);
+		}
+		if(temp->rchild!=NULL){
+			BTreelayer* temp2=new BTreelayer;
+			temp2->layer=k+1;
+			temp2->p=temp->rchild;
+			q.push(temp2);
+		}
+	}
+	int maxwidth=-1;
+	for(int i=1;i<=bt->layer;i++){
+		if(layer[i]>maxwidth){
+			maxwidth=layer[i];
+		}
+	}
+	return maxwidth;	
+}
+
+void preorderTowpl(BTNode *root,int deep,int &sum){//递归计算二叉树的WPL
+	if(root==NULL)return;
+	if(root->lchild==NULL&&root->rchild==NULL){
+		sum+=(root->data)*deep;
+	}
+	preorderTowpl(root->lchild,deep+1,sum);
+	preorderTowpl(root->rchild,deep+1,sum);
+}
+
+int height(BTNode* bt){//递归求树高度
+	if(bt==NULL)return 0;
+	hl=height(bt->lchild);
+	hr=height(bt->rchild);
+	return (hl>hr?hl:h2)+1;
+}
+
+int leaves(BTNode *bt){
+	if(bt==NULL)return 0;
+	return leaves(bt->lchild)+leaves(bt->rchild);
+}
 int main(){
-	int A[5]={4,2,5,1,3};
+	int A[6]={4,2,5,1,3,6};
 	BTNode *root=createTree(A);
 	//FindK(root,5);
 	//postordernorecursion(root);
-	printancestorpath(root,3);
+	//printancestorpath(root,3);
 	//nearestcommonancestor(root,1,2);
+	/* int width=getwidth(root);
+	cout<<width;
+	int sum=0;
+	preorderTowpl(root,0,sum);
+	cout<<sum; */
+	cout<<height(bt);
 	return 0;
 }
